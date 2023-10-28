bf42845f820f8120670ff4a96e98e21d3ea0b4fe
unknown <hamzabenrahhou3@gmail.com>
2023-10-28 12:53:48 -0700

    add table attribute to modal

--- src/components/common/ModelTable.tsx
+++ src/components/common/ModelTable.tsx
@@ -4,36 +4,6 @@ import { Menu, Transition } from '@headlessui/react'
 
 import { colors } from '../../data/DummyData';
 
-const people = [
-    {
-        name: 'Lindsay Walton',
-        title: 'Front-end Developer',
-        department: 'Optimization',
-        email: 'lindsay.walton@example.com',
-        role: 'Member',
-        image:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Lindsay Walton',
-        title: 'Front-end Developer',
-        department: 'Optimization',
-        email: 'lindsay.walton@example.com',
-        role: 'Member',
-        image:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Lindsay Walton',
-        title: 'Front-end Developer',
-        department: 'Optimization',
-        email: 'lindsay.walton@example.com',
-        role: 'Member',
-        image:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    // More people...
-]
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(' ')
@@ -42,7 +12,7 @@ function classNames(...classes: any) {
 export default function ModelTable(props: any) {
     const [tableName, setTableName] = useState(props.tableTarget.Name);
     const [tableColor, setTableColor] = useState('#ffffff');
-
+    console.log(props.tableTarget)
     const updateTableName = (event: any) => {
         setTableName(event.target.value);
     }
@@ -73,7 +43,7 @@ export default function ModelTable(props: any) {
                     </div>
                     <Menu as="div" className="relative inline-block text-left">
                         <div className='h-full flex my-auto items-center'>
-                            <Menu.Button style={{backgroundColor:tableColor}} className="inline-flex w-9 h-9 justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                            <Menu.Button style={{ backgroundColor: tableColor }} className="inline-flex w-9 h-9 justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
 
                             </Menu.Button>
                         </div>
@@ -92,8 +62,8 @@ export default function ModelTable(props: any) {
                                     {
                                         colors.map((color) => (
                                             <Menu.Item key={color}>
-                                                <div className=' flex w-8 h-8 border border-gray-500 rounded-md my-1 content-center m-auto cursor-pointer'  onClick={changeTableColor}>
-                                                    <div style={{backgroundColor:color}} className={`w-6 h-6 m-auto items-center rounded-md flex`}></div>
+                                                <div className=' flex w-8 h-8 border border-gray-500 rounded-md my-1 content-center m-auto cursor-pointer' onClick={() => setTableColor(color)}>
+                                                    <div style={{ backgroundColor: color }} className={`w-6 h-6 m-auto items-center rounded-md flex`}></div>
                                                 </div>
                                             </Menu.Item>
                                         ))
@@ -115,13 +85,13 @@ export default function ModelTable(props: any) {
                                         Name
                                     </th>
                                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Title
+                                        Type
                                     </th>
                                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                         Status
                                     </th>
                                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Role
+                                        Constraints
                                     </th>
                                     <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                                         <span className="sr-only">Edit</span>
@@ -129,33 +99,27 @@ export default function ModelTable(props: any) {
                                 </tr>
                             </thead>
                             <tbody className="divide-y divide-gray-200 bg-white">
-                                {people.map((person) => (
-                                    <tr key={person.email}>
+                                {props.tableTarget.Attributes.map((attribute: any) => (
+                                    <tr key={attribute.Name}>
                                         <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                                             <div className="flex items-center">
-                                                <div className="h-11 w-11 flex-shrink-0">
-                                                    <img className="h-11 w-11 rounded-full" src={person.image} alt="" />
-                                                </div>
                                                 <div className="ml-4">
-                                                    <div className="font-medium text-gray-900">{person.name}</div>
-                                                    <div className="mt-1 text-gray-500">{person.email}</div>
+                                                    <div className="font-medium text-gray-900">{attribute.Name}</div>
                                                 </div>
                                             </div>
                                         </td>
                                         <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                                            <div className="text-gray-900">{person.title}</div>
-                                            <div className="mt-1 text-gray-500">{person.department}</div>
+                                            <div className="text-gray-900">{attribute.Type}</div>
                                         </td>
                                         <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                                             <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                                                 Active
                                             </span>
                                         </td>
-                                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.role}</td>
                                         <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                Edit<span className="sr-only">, {person.name}</span>
-                                            </a>
+                                            <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                                Active
+                                            </span>
                                         </td>
                                     </tr>
                                 ))}
